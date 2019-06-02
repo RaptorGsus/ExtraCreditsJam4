b50514962e72c07c9c9a70b00acaf1ed5c60a0ee
Jeff Kloos <jeffrey.kloos@gmail.com>
2019-06-02 16:33:32 +0200

    First Version of Switchboard Mechanic

---
+++ TelephoneOperator/Assets/CallerCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CallerCard : MonoBehaviour
+{
+    public TextMeshProUGUI nameTextMesh;
+    public TextMeshProUGUI numberTextMesh;
+    public TextMeshProUGUI infoTextMesh;
+
+    private void Start()
+    {
+        
+    }
+}
